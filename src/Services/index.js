545c64f64a6f3498172177825eb277e6534965eb
kailash yadav <kailashyadav1998@gmail.com>
2023-08-07 01:33:41 +0530

    updated code

--- src/Services/index.js
+++ src/Services/index.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const apiData = axios.create({
-	baseURL: 'http://localhost:3000',
-    withCredentials:false,
-    headers: {
-        Accept:'application/json',
-        ContentType:"application/json"
-    }
+	baseURL: 'https://my-json-server.typicode.com/band_bucket/mens-watch',
+	// baseURL: 'http://localhost:3000',
+	withCredentials: false,
+	headers: {
+		Accept: 'application/json',
+		ContentType: 'application/json',
+	},
 });
 
 export default {
